f0c3441b170d37eca9da73ecf43e8b4879b94351
Tomjerri1 <151880307+Tomjerri1@users.noreply.github.com>
2025-09-29 22:19:52 +0300

    Add store class and it's repository, service and controller.

---
+++ lab1/lab1/src/main/java/edu/maksimchuk/lab1/ShopAppApplication.java
@@ -0,0 +1,18 @@
+package edu.maksimchuk.lab1;
+
+/**
+ * @author Tom
+ * @version 1.0.0
+ * @project lab1
+ * @class ShopAppApplication
+ * @since 29.09.2025 - 22.13
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAppApplication.class, args);
+    }
+}
